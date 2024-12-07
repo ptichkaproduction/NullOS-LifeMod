af3f6da088d07271c6add7d7c6656e5cf81e926b
Артём Межинский <aswert092@gmail.com>
2024-12-07 21:04:16 +0300

    minor code edits

--- NullOS/boot.cs
+++ NullOS/boot.cs
@@ -1,6 +1,16 @@
-﻿using System;
+﻿//
+// Created by aswer 
+//
+// at 26.10.2024
+//
+// TODO:
+//
+//
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,8 +22,8 @@ namespace NullOS
         {
             try
             {
-                Console.ForegroundColor = ConsoleColor.DarkGray; Console.Write("BOOTLOADER"); Console.ForegroundColor = ConsoleColor.White; Console.Write(": ["); Console.ForegroundColor = ConsoleColor.Green; Console.Write("SUCCESS"); Console.ForegroundColor = ConsoleColor.White; Console.Write("] LOADED.\n");
                 var core = new core();
+                Console.ForegroundColor = ConsoleColor.DarkGray; Console.Write("BOOTLOADER"); Console.ForegroundColor = ConsoleColor.White; Console.Write(": ["); Console.ForegroundColor = ConsoleColor.Green; Console.Write("SUCCESS"); Console.ForegroundColor = ConsoleColor.White; Console.Write("] LOADED.\n");
                 core.core_main();
                 Console.ForegroundColor = ConsoleColor.DarkGray; Console.Write("BOOTLOADER"); Console.ForegroundColor = ConsoleColor.White; Console.Write(": ["); ; Console.ForegroundColor = ConsoleColor.Magenta; Console.Write("EXITED"); Console.ForegroundColor = ConsoleColor.White; Console.Write("] CODE: 0 ["); Console.ForegroundColor = ConsoleColor.DarkYellow; Console.Write("0x0"); Console.ForegroundColor = ConsoleColor.White; Console.Write("]\n");
             }
